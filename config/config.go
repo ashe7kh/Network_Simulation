d72bfd106d62bc8e145d7f6d82e3221cbdd8ef88
Aquil Sheikh <sheikhaq@bc.edu>
2020-09-12 12:30:30 -0400

    Addded to config.go file, filled out most of ReadFile function body, still figuring it out. Committing for later meeting with team

--- config/config.go
+++ config/config.go
@@ -1,23 +1,46 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
-type config struct{
-	ID int
+type config struct {
+	ID   int
 	port string
-	IP string
+	IP   string
 	minD int
 	maxD int
 }
 
-func ReadFile config[]{
-
+func ReadFile(ConfigFile string) []config {
 	type minD int
+
 	type maxD int
-	
+
+	//open and read config file
+	ConfigFile, err := os.Open("config.txt") //assuming this will be file name
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer ConfigFile.Close()
+
+	//read the files contents
+	scanner := bufio.NewScanner(ConfigFile)
+	//parse out each word individually
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		//parse out details of the config file to then populate the necessary fields
+		fmt.Println(scanner.Text())
+
+	}
 	//pseudocode
 	/*
-	parse line 1 from file and populate minD and maxD
-	readfile by line: AFTER line 1 break each line into 3 strings: populate fields from this, and set minD and maxD
+	   parse line 1 from file and populate minD and maxD
+	   readfile by line: AFTER line 1 break each line into 3 strings: populate fields from this, and set minD and maxD
 
-	 */
+	*/
+	return nil // finish writing return statement
 }
