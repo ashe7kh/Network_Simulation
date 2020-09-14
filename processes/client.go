d66cd6f8dc23e59e50b935acb48865dbb111f209
ashe7kh <47406472+ashe7kh@users.noreply.github.com>
2020-09-14 19:44:59 -0400

    Update client.go

--- processes/client.go
+++ processes/client.go
@@ -8,100 +8,101 @@ import (
 	"os"
 	"strings"
 	"time"
-	"../config"
-
 )
 
-type cfile = config.Config
 
-type message struct{
-	Content string
-	Time string
-}
-
-func printMessage(ID int, Message message){ //function that prints the email on the client side
+//function that prints the email on the client side
+//prived time that the message was sent
+func printMessage(ID int, Message message){
 	fmt.Println("\n -------------------------- \n ---Message Confirmed Sent--- \n -------------------------- \n")
-	fmt.Println("Message Sent to Process 2: " )
+	fmt.Println("Message Sent to:" + string(ID))//add the unique id from config file
 	fmt.Println("Message Content: " + Message.Content)
 	t := time.Now()
-	timeSent := t.Format(time.RFC3339)
+	timeSent := t.Format(time.RFC850)
+	fmt.Println("Confirmed sent at: " + timeSent)
 	timeSent = Message.Time
-	fmt.Println("Confirmed sent at: " + Message.Time)
 	fmt.Println("\n -------------------------- \n")
 }
-/*
-func MessageToString(Message message) string{ //function that converts the email to a string
-   var s string
-   s += Message.Content + "\n"
-   return s
-}
 
-/*
-func MessageDelay(Config cfile, Message message) string {
-   min := Config.MinD
-   max := Config.MaxD
-   //delay := make(chan int)
+func UnicastSend(config cfile, m message){
 
-   //var d time.Duration = time.Duration(n * int(time.Millisecond))
-   //tick := time.Tick(d)
-   text := MessageToString(Message)
-
-   return text
-
-
-}
-
-*/
-
-func unicastSend(config cfile, m message) message{
-	address := config.IP + ":" + config.port
-
+	//obtain desired identifiers as well as IP addresses from config file
+	//establish desired connection from config parameters
+	//creates TCP connection on the client side
+	address := config.IP + ":" + config.Port
 	conn, err := net.Dial("tcp", address)
 	if err != nil {
 		panic(err)
 	}
+
 	defer conn.Close()
 
-	fmt.Fprintf(conn, m.Content + "\n")
+	//termination protocol
+	if strings.TrimSpace(m.Content) != "END" {
+		//send and then print the message on client APP side
+		//todo have time be updated dynamically with the sending of the message not immediately after
+		fmt.Fprintf(conn, m.Content+"\n")
+		printMessage(config.ID, m)
 
-	printMessage(m)
-	return m
+	} else { //end communication
+		fmt.Println("Exiting TCP Client")
+		return
+	}
 
 }
 
+//will extract the message itself from command line and then add delay prior to sending
+func MessageParse(config cfile, text string){
+	var c cfile
+	var m message
 
-func main(){
-	//declare type of connection as well as the port from which it will occur
 
-	for {
-		var c cfile
+	for{
 
-		var m message
-		timeChannel := make(chan int)
-		//prompt user to construct email then read email
-		//TODO add feature to detect incorrect format and provide instruction to correct
-		//read what was written on the command line
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Please enter your Message:")
-		text, _ := reader.ReadString('\n')
+		//create a string array to then parse out the message from the ID and IP info
+		input := strings.Split(text, " ")
 
-		//termination protocol
-		if strings.TrimSpace(text) == "END"{
-			fmt.Println("Exiting TCP Client")
-			return
-		}
+		//we only care about the information after declarations
+		MessageActual := input[2:]
+
+		//convert the array to a simple string
+		text := strings.Join(MessageActual, " ")
 
 		//fill structure with message content
 		m.Content = text
 
-		//add a delay via message delay function
-		min := 1//c.MinD
-		max := 10//c.MaxD
+		//add a delay via timer and AfterFunc
+		//we determine delay amount by recalling the information from config file
+		min := c.MinD
+		max := c.MaxD
 		n := rand.Intn(max-min) + min
-		timer := time.Duration(n)*time.Millisecond
-		time.AfterFunc(time.Duration(timer),func(){
-			unicastSend(c, m)
-			})
+		timer := time.Duration(n)* time.Millisecond
+		time.AfterFunc(timer, func(){
+			UnicastSend(c, m)
+		})
 
 	}
+
+}
+
+
+
+func main(){
+	var c cfile
+	//var m message
+	c = readF("config.txt")[1] //two processes, send to process 2 => ID = 2 => index = 1
+
+	//prompt user to construct message then read message
+	//TODO add feature to detect incorrect format and provide instruction to correct
+	//read what was written on the command line
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Please enter your Message:")
+
+	//convert text to a string
+	text, _ := reader.ReadString('\n')
+
+	//then send the text to a parser function to only send desired information
+	//parser also implements a delay in the form of a timer
+	MessageParse(c, text)
+
 }
