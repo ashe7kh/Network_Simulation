63e4f32119cf102dbe02f5fd274d7428d71f613b
ashe7kh <47406472+ashe7kh@users.noreply.github.com>
2020-09-13 23:59:53 -0400

    Update client.go

--- processes/client.go
+++ processes/client.go
@@ -3,40 +3,105 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"net"
 	"os"
+	"strings"
+	"time"
+	"../config"
+
 )
 
-func main(){
-	//declare type of connection as well as the port from which it will occur
-	conn, err := net.Dial("tcp", "localhost:9000")
+type cfile = config.Config
+
+type message struct{
+	Content string
+	Time string
+}
+
+func printMessage(ID int, Message message){ //function that prints the email on the client side
+	fmt.Println("\n -------------------------- \n ---Message Confirmed Sent--- \n -------------------------- \n")
+	fmt.Println("Message Sent to Process 2: " )
+	fmt.Println("Message Content: " + Message.Content)
+	t := time.Now()
+	timeSent := t.Format(time.RFC3339)
+	timeSent = Message.Time
+	fmt.Println("Confirmed sent at: " + Message.Time)
+	fmt.Println("\n -------------------------- \n")
+}
+/*
+func MessageToString(Message message) string{ //function that converts the email to a string
+   var s string
+   s += Message.Content + "\n"
+   return s
+}
+
+/*
+func MessageDelay(Config cfile, Message message) string {
+   min := Config.MinD
+   max := Config.MaxD
+   //delay := make(chan int)
+
+   //var d time.Duration = time.Duration(n * int(time.Millisecond))
+   //tick := time.Tick(d)
+   text := MessageToString(Message)
+
+   return text
+
+
+}
+
+*/
+
+func unicastSend(config cfile, m message) message{
+	address := config.IP + ":" + config.port
+
+	conn, err := net.Dial("tcp", address)
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
 
-	for {
+	fmt.Fprintf(conn, m.Content + "\n")
 
+	printMessage(m)
+	return m
+
+}
+
+
+func main(){
+	//declare type of connection as well as the port from which it will occur
+
+	for {
+		var c cfile
+
+		var m message
+		timeChannel := make(chan int)
 		//prompt user to construct email then read email
 		//TODO add feature to detect incorrect format and provide instruction to correct
-		//read what was written on the command line then send it through connection previosly established
+		//read what was written on the command line
 		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Please enter your email with format To; From; Title; Content: ")
+		fmt.Print("Please enter your Message:")
 		text, _ := reader.ReadString('\n')
-		fmt.Fprintf(conn, text+"\n")
 
-		//receiving confirmation from server
-		//the client then receives a copy of their email as confirmation of its delivery
-		message, _ := bufio.NewReader(conn).ReadString('|')
-		fmt.Print("\n" + message)
-
-		//disconection protocol
-		if len(message) >1  {
-			fmt.Println("TCP client exiting...")
+		//termination protocol
+		if strings.TrimSpace(text) == "END"{
+			fmt.Println("Exiting TCP Client")
 			return
 		}
 
+		//fill structure with message content
+		m.Content = text
 
+		//add a delay via message delay function
+		min := 1//c.MinD
+		max := 10//c.MaxD
+		n := rand.Intn(max-min) + min
+		timer := time.Duration(n)*time.Millisecond
+		time.AfterFunc(time.Duration(timer),func(){
+			unicastSend(c, m)
+			})
 
 	}
 }
