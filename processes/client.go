a04cdf949a258dc2eadf2b247f7a759d9e384db5
ashe7kh <47406472+ashe7kh@users.noreply.github.com>
2020-09-15 22:17:02 -0400

    Update client.go

--- processes/client.go
+++ processes/client.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"../config"
+	"../message"
 	"bufio"
 	"fmt"
 	"math/rand"
@@ -10,21 +12,23 @@ import (
 	"time"
 )
 
+type confile = config.Config
+type Message = message.Message
 
 //function that prints the email on the client side
 //prived time that the message was sent
-func printMessage(ID int, Message message){
+func printMessage(ID int, message Message) {
 	fmt.Println("\n -------------------------- \n ---Message Confirmed Sent--- \n -------------------------- \n")
-	fmt.Println("Message Sent to:" + string(ID))//add the unique id from config file
-	fmt.Println("Message Content: " + Message.Content)
+	fmt.Println("Message Sent to:" + string(ID)) //add the unique id from config file
+	fmt.Println("Message Content: " + message.Content)
 	t := time.Now()
 	timeSent := t.Format(time.RFC850)
 	fmt.Println("Confirmed sent at: " + timeSent)
-	timeSent = Message.Time
+	timeSent = message.Time
 	fmt.Println("\n -------------------------- \n")
 }
 
-func UnicastSend(config cfile, m message){
+func UnicastSend(config confile, m Message) {
 
 	//obtain desired identifiers as well as IP addresses from config file
 	//establish desired connection from config parameters
@@ -52,12 +56,11 @@ func UnicastSend(config cfile, m message){
 }
 
 //will extract the message itself from command line and then add delay prior to sending
-func MessageParse(config cfile, text string){
-	var c cfile
-	var m message
+func MessageParse(config confile, text string) {
+	var c = config
+	var m Message
 
-
-	for{
+	for {
 
 		//create a string array to then parse out the message from the ID and IP info
 		input := strings.Split(text, " ")
@@ -73,11 +76,13 @@ func MessageParse(config cfile, text string){
 
 		//add a delay via timer and AfterFunc
 		//we determine delay amount by recalling the information from config file
-		min := c.MinD
-		max := c.MaxD
-		n := rand.Intn(max-min) + min
-		timer := time.Duration(n)* time.Millisecond
-		time.AfterFunc(timer, func(){
+		//min := c.MinD
+		//max := c.MaxD
+		n := rand.Intn(config.MaxD-config.MinD) + config.MinD
+
+		timer := time.Duration(n) * time.Millisecond
+
+		time.AfterFunc(timer, func() {
 			UnicastSend(c, m)
 		})
 
@@ -85,12 +90,10 @@ func MessageParse(config cfile, text string){
 
 }
 
-
-
-func main(){
-	var c cfile
+func main() {
+	var c confile
 	//var m message
-	c = readF("config.txt")[1] //two processes, send to process 2 => ID = 2 => index = 1
+	c = config.ReadFile("config.txt")[1] //two processes, send to process 2 => ID = 2 => index = 1
 
 	//prompt user to construct message then read message
 	//TODO add feature to detect incorrect format and provide instruction to correct
