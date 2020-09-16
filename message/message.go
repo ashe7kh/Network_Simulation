14a05057e5b57fa3bc4fe2ceb19f6bad09158594
ashe7kh <47406472+ashe7kh@users.noreply.github.com>
2020-09-15 22:20:07 -0400

    Create message.go

---
+++ message/message.go
@@ -0,0 +1,6 @@
+package message
+
+type Message struct { //this is message, we named it differently since we use message in server and Message in client
+	Content string  //also it's funny
+	Time    string
+}
