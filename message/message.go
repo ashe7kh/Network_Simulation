6dd0722a1efd852c6ca1343db90aedb86928505c
ashe7kh <47406472+ashe7kh@users.noreply.github.com>
2020-09-18 18:37:35 -0400

    Update message.go

--- message/message.go
+++ message/message.go
@@ -1,6 +1,7 @@
 package message
 
-type Message struct { //this is message, we named it differently since we use message in server and Message in client
-	Content string  //also it's funny
-	Time    string
+type Message struct {
+	Content  string
+	Time     string //time sent
+	Local_ID int
 }
